0af8cca3f1f9bd766c8319c14da371fd378c7f3f
Daria <daria.kiseleva@mail.mcgill.ca>
2022-10-11 18:33:07 -0400

    Write the create_app application factory function

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,5 +3,40 @@ import os
 from flask import Flask
 
 
+# ---The "application factory" function which return the app
+# (For configuration, registration, and other setup)
+
+
 def create_app(test_config=None):
-    pass
+    # Create flask instance
+    # __name__ = name of current Python module
+    # True -> Configuration files are relative to the instance folder (outside the flaskr package)
+    app = Flask(__name__, instance_relative_config=True)
+
+    # Default configuration of the app
+    # Secret key keeps data safe, 'dev' should be overriden with a random value when deploying
+    # The SQLite database is under the Flask instance folder
+    app.config.from_mapping(
+        SECRET_KEY="dev", DATABASE=os.path.join(app.instance_path, "flaskr.sqlite")
+    )
+
+    if test_config is None:
+        # If not testing, load the instance config (override any values) if it exists
+        # (For example, load a real SECRET_KEY when deploying)
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # If the test config (for testing the app) is passed in, load it
+        app.config.from_mapping(test_config)
+
+    # If the instance folder does not exist, create it, otherwise pass
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # --Simple page
+    @app.route("/hello")
+    def hello():
+        return "Hello, world!"
+
+    return app
