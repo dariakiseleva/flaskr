d3dee6eeb46865433efab09a2fc2fa35f24a78b9
Daria <daria.kiseleva@mail.mcgill.ca>
2022-10-11 23:47:57 -0400

    Create blog.index view and template

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -6,3 +6,15 @@ from flaskr.db import get_db
 
 # Blog does not have a url prefix, as is the main feature
 bp = Blueprint("blog", __name__)
+
+
+@bp.route("/")
+def index():
+    # Extract post data and pass it to a template
+    db = get_db()
+    posts = db.execute(
+        "SELECT p.id, title, body, created, author_id, username"
+        " FROM post p JOIN user u ON p.author_id=u.id"
+        " ORDER BY created DESC"
+    ).fetchall()
+    return render_template("blog/index.html", posts=posts)
